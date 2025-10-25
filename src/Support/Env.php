d7ea0f6a93f5d6f7484046e336a1f2ef4cdb33dc
Бошко Радивојевић <131773254+BRadivojevic@users.noreply.github.com>
2025-10-25 15:14:20 +0200

    Delete src directory

--- src/Support/Env.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Support;
-
-final class Env {
-	public static function get(string $key, ?string $default = null): ?string {
-		return $_ENV[$key] ?? getenv($key) ?: $default;
-	}
-}
