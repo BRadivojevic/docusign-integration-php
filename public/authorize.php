0e4b0a28f6d089fc9cd57f37b441ffef178eecf4
Бошко Радивојевић <131773254+BRadivojevic@users.noreply.github.com>
2025-10-25 15:14:09 +0200

    Delete public directory

--- public/authorize.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-use App\OAuth\Authorize;
-header('Location: '.Authorize::url());
--- public/send.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__.'/../vendor/autoload.php';
-use App\Api\SendEnvelope;
-$token = $_GET['access_token'] ?? '';
-$email = $_GET['email'] ?? 'test@example.com';
-SendEnvelope::send($token, $email);
